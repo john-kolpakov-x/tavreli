2db80364440643020cb2c3905a34fc563214467f
pompei <ekolpakov@greet-go.com>
2017-10-09 23:40:36 +0600

    Скопирован клиент из sandbox-а

---
+++ client/front/ts/model/UserInfo.ts
@@ -0,0 +1,16 @@
+export class UserInfo {
+  public id: string;
+  public accountName: string;
+  public surname: string | null;
+  public name: string | null;
+  public patronymic: string | null;
+
+  public assign(o: UserInfo): UserInfo {
+    this.id = o.id;
+    this.accountName = o.accountName;
+    this.surname = o.surname;
+    this.name = o.name;
+    this.patronymic = o.patronymic;
+    return this;
+  }
+}
